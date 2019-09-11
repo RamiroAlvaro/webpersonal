552fd1919468d77d4c35c91a98e08b419b919604
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2019-09-11 18:16:37 -0300

    Modelo Project

---
+++ webpersonal/portfolio/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from webpersonal.portfolio.models import Project
+
+
+admin.site.register(Project)
--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'webpersonal.core',
+    'webpersonal.portfolio',
 ]
 
 MIDDLEWARE = [
