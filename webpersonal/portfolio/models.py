99c01f4f8f2757d2f5696e63000a53771676be3b
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2019-09-12 13:20:48 -0300

    Agrega tests

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField('Título', max_length=200)
     description = models.TextField('Descripción')
     image = models.ImageField('Imagen', upload_to='projects')
+    link = models.URLField('Dirección web', blank=True, null=True)
     created = models.DateTimeField('Fecha de creación', auto_now_add=True)
     updated = models.DateTimeField('Fecha de edición', auto_now=True)
 
